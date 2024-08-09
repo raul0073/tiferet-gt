3e97a3c7ee59955efc58477974b1741309e38c35
raul0073 <razmassami@gmail.com>
2024-08-09 12:18:45 +0300

    gurad update

--- client/src/app/auth/auth.guard.ts
+++ client/src/app/auth/auth.guard.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../services/auth.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-
+  // if logged
   if (authService.isLoggedIn()) {
     return true;
   } else {
